55142e2212fc177a3638f9ede4be63756295a43e
LuisM11 <lumarinb@unal.edu.co>
2023-07-09 22:43:37 -0500

    Springboot project init, first dependencies

---
+++ src/main/java/com/marinb/quizystemapi/QuizystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.marinb.quizystemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizystemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marinb/quizystemapi/QuizystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marinb.quizystemapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
